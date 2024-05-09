1a9753ca51bf585cc7ea4544cd3262c90c4dbe50
Adamlimnz <adamlimnz@gmail.com>
2024-05-10 11:05:20 +1200

    Created a form where you can add scores to gymnasts, which then get added to the database

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 
@@ -29,7 +29,45 @@ def gymnast():
 #creating a page to add scores
 @app.route('/addscores')
 def scores():
-    return render_template("score.html")
+
+    #viewing gymnasts that are in the database
+    conn = sqlite3.connect('database')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM gymnast")
+    data = cur.fetchall()
+    conn.close()
+
+
+    #viewing apparatus with their id's
+    conn = sqlite3.connect('database')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM apparatus")
+    appdata = cur.fetchall()
+    conn.close()
+
+
+    #getting the gymnast id, apparatus id, their e-score and their d-score
+    form1 = None
+    form2 = None
+    form3 = None
+    form4 = None
+    if len(request.args) > 0:
+        form1 = request.args.get('addgymnastid')
+        form2 = request.args.get('app')
+        form3 = request.args.get('add-escore')
+        form4 = request.args.get('add-dscore')
+        
+
+    #adding the new scores
+    conn = sqlite3.connect('database')
+    cur = conn.cursor()
+    sql = ("INSERT INTO score (gymnast_id, apparatus_id, escore, dscore) VALUES (?,?,?,?)")
+    cur.execute(sql, (form1, form2, form3, form4))
+    conn.commit()
+    conn.close()
+
+    return render_template("score.html", appdata=appdata, data=data)
+
 
 #creating a page to view leaderboards
 @app.route('/leaderboard')
