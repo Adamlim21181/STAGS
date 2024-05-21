adf44bf7be14a5ed7603e0f7ab923bd2ad2b8fe7
Adamlimnz <adamlimnz@gmail.com>
2024-05-22 10:20:46 +1200

    improved the javascript code for the dropdown tables

--- static/myscript.js
+++ static/myscript.js
@@ -3,30 +3,6 @@ const cleanUri = window.location.href.split('?')[0];
     window.history.replaceState({}, document.title, cleanUri);
 
 /* When the button is clicked switch between hiding and showing the dropdown content */
-function alldrp() {
-    document.getElementById("allaround").classList.toggle("show");
-}
-
-function floordrp() {
-  document.getElementById("floor").classList.toggle("show");
-}
-
-function pommeldrp() {
-  document.getElementById("pommel").classList.toggle("show");
-}
-
-function ringsdrp() {
-  document.getElementById("rings").classList.toggle("show");
-}
-
-function vaultdrp() {
-  document.getElementById("vault").classList.toggle("show");
-}
-
-function pbarsdrp() {
-  document.getElementById("pbars").classList.toggle("show");
-}
-
-function highbardrp() {
-  document.getElementById("highbar").classList.toggle("show");
+function toggleDropdown(dropdownId) {
+    document.getElementById(dropdownId).classList.toggle("show");
 }
