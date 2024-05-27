e528eb62b11cdd57f8b9aa215659cf6a99fa65b8
Adamlimnz <adamlimnz@gmail.com>
2024-05-27 12:39:40 +1200

    added a few more code comments

--- routes.py
+++ routes.py
@@ -12,12 +12,13 @@ def homepage():
 #creating a page to add gymnasts
 @app.route('/addgymnast')
 def gymnast():
+
+    #adding gymnasts to the database
     form = None
     if len(request.args) > 0:
         #getting the gymnast name from the websites input
         form = request.args.get('registorname')
         if form is not None:
-            #adding the gymnast name to the database
             conn = sqlite3.connect('database')
             cur = conn.cursor()
             sql = ("INSERT INTO gymnast (gymnast_name) VALUES(?)")
@@ -32,16 +33,14 @@ def gymnast():
     regdata = cur.fetchall()
     conn.close()
 
-    
+    #Editing registered gymnasts
     id = None
     name = None
     if len(request.args) > 0:
         #getting the registered gymnasts id and new name
         id = request.args.get('id')
         name = request.args.get('newname')
-
         if id is not None and name is not None: 
-            #replacing the old gymnast name with the new gymast name
             conn = sqlite3.connect('database')
             cur = conn.cursor()
             sql = ("UPDATE gymnast SET gymnast_name = ? WHERE gymnast_id = ?")
@@ -69,49 +68,50 @@ def scores():
     appdata = cur.fetchall()
     conn.close()
 
-    #getting the gymnast id, apparatus id, their e-score and their d-score
+    #Adding Scores
     form1 = None
     form2 = None
     form3 = None
     form4 = None
     if len(request.args) > 0:
+        #getting the data needed from the users input
         form1 = request.args.get('addgymnastid')
         form2 = request.args.get('app')
         form3 = request.args.get('add-escore')
         form4 = request.args.get('add-dscore')
+        if form1 is not None and form2 is not None and form3 is not None and form4 is not None:
+            conn = sqlite3.connect('database')
+            cur = conn.cursor()
+            sql = ("INSERT INTO score (gymnast_id, apparatus_id, escore, dscore) VALUES (?,?,?,?)")
+            cur.execute(sql, (form1, form2, form3, form4))
+            conn.commit()
+            conn.close()
 
-    #adding the new scores
-    if form1 is not None and form2 is not None and form3 is not None and form4 is not None:
-        conn = sqlite3.connect('database')
-        cur = conn.cursor()
-        sql = ("INSERT INTO score (gymnast_id, apparatus_id, escore, dscore) VALUES (?,?,?,?)")
-        cur.execute(sql, (form1, form2, form3, form4))
-        conn.commit()
-        conn.close()
-
+    #Viewing the registered scores
     conn = sqlite3.connect('database')
     cur = conn.cursor()
     cur.execute("SELECT score_id, score.gymnast_id, gymnast.gymnast_name, apparatus.apparatus_name, escore, dscore FROM score INNER JOIN gymnast ON score.gymnast_id=gymnast.gymnast_id INNER JOIN apparatus ON score.apparatus_id=apparatus.apparatus_id ")
     scoredata = cur.fetchall()
     conn.close()
 
+    #Editing the registered scores
     newform1 = None
     newform2 = None
     newform3 = None
     newform4 = None
     if len(request.args) > 0:
+        #Getting the data needed from the users input
         newform1 = request.args.get('scoreid')
         newform2 = request.args.get('appid')
         newform3 = request.args.get('newescore')
         newform4 = request.args.get('newdscore')
-
-    if newform1 is not None and newform2 is not None and newform3 is not None and newform4 is not None:
-        conn = sqlite3.connect('database')
-        cur = conn.cursor()
-        sql = ("UPDATE score SET apparatus_id = ?, escore = ?, dscore = ? WHERE score_id = ?")
-        cur.execute(sql, (newform2, newform3, newform4, newform1))
-        conn.commit()
-        conn.close()
+        if newform1 is not None and newform2 is not None and newform3 is not None and newform4 is not None:
+            conn = sqlite3.connect('database')
+            cur = conn.cursor()
+            sql = ("UPDATE score SET apparatus_id = ?, escore = ?, dscore = ? WHERE score_id = ?")
+            cur.execute(sql, (newform2, newform3, newform4, newform1))
+            conn.commit()
+            conn.close()
 
     return render_template("score.html", appdata=appdata, gymdata=gymdata, scoredata=scoredata)
 
