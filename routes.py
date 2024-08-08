a0157f7cf924f80fba0b8f1d4daa9a42d5b3cb96
Adamlimnz <adamlimnz@gmail.com>
2024-08-08 17:09:08 +1200

    Added a logo, changed the leaderboard, added a 404 error page, fixed some mistakes

--- routes.py
+++ routes.py
@@ -1,244 +1,265 @@
 from flask import Flask, render_template, request
-import sqlite3, time
-
+import sqlite3
+import time
 
 app = Flask(__name__)
 
-#creating a hompage
-@app.route('/')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.route("/")
 def homepage():
     return render_template("home.html")
 
-#creating a page to add gymnasts
-@app.route('/addgymnast')
+
+@app.route("/addgymnast")
 def gymnast():
 
-    #adding gymnasts to the database
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+
     form1 = None
     form2 = None
-    if len(request.args) > 0:
-        #getting the gymnast name from the websites input
-        form1 = request.args.get('registorname')
-        form2 = request.args.get('registorlevel')
-        if form1 is not None and form2 is not None:
-            conn = sqlite3.connect('database.db')
-            cur = conn.cursor()
-            sql = ("INSERT INTO gymnast (gymnast_name, level) VALUES(?,?)")
-            cur.execute(sql, (form1, form2))
+    if request.args:
+        form1 = request.args.get("registorname")
+        form2 = request.args.get("registorlevel")
+        if form1 and form2:
+            cur.execute(
+                "INSERT INTO gymnast (gymnast_name, level) VALUES (?, ?)",
+                (form1, form2),
+                )
             conn.commit()
-            conn.close()
 
-    #viewing registered gymnasts
-    conn = sqlite3.connect('database.db')
-    cur = conn.cursor()
     cur.execute("SELECT * FROM gymnast")
     regdata = cur.fetchall()
-    conn.close()
 
-    #delete registered gymnasts
-    delete = None
-    delete_error = None
-    if len(request.args) > 0:
-        delete = request.args.get('deleteid')
-        if delete is not None:
-            conn = sqlite3.connect('database.db')
-            cur = conn.cursor()
-
-            # Check if ID exists
-            cur.execute("SELECT * FROM gymnast WHERE gymnast_id = ?", (delete,))
+    delete = delete_error = None
+    if request.args:
+        delete = request.args.get("deleteid")
+        if delete:
+            cur.execute(
+                "SELECT * FROM gymnast WHERE gymnast_id = ?",
+                (delete,)
+                )
             if not cur.fetchone():
-                delete_error = "Error: id does not exist."
+                delete_error = "Error: ID does not exist."
+            else:
+                time.sleep(1)
+                cur.execute(
+                    "DELETE FROM gymnast WHERE gymnast_id = ?", (delete,)
+                )
+                conn.commit()
 
-            time.sleep(1)
-            sql = ("DELETE FROM gymnast WHERE gymnast_id = ?")
-            cur.execute(sql, (delete,))
-            conn.commit()
-            conn.close()
-    
-    #Editing registered gymnasts
     id = None
     name = None
     level = None
     error_message = None
-    if len(request.args) > 0:
-        #getting the registered gymnasts id and new name
-        id = request.args.get('id')
-        name = request.args.get('newname')
-        level = request.args.get('newlevel')
-        if id is not None and name is not None and level is not None: 
-            conn = sqlite3.connect('database.db')
-            cur = conn.cursor()
-            sql = ("UPDATE gymnast SET gymnast_name = ?, level = ? WHERE gymnast_id = ?")
-            cur.execute(sql, (name, level, id))
+    if request.args:
+        id = request.args.get("id")
+        name = request.args.get("newname")
+        level = request.args.get("newlevel")
+        if id and name and level:
+            cur.execute(
+                "UPDATE gymnast SET "
+                "gymnast_name = ?, "
+                "level = ? "
+                "WHERE gymnast_id = ?",
+                (name, level, id),
+                )
+
             conn.commit()
-
-            # Check if ID exists
-            cur.execute("SELECT * FROM gymnast WHERE gymnast_id = ?", (id,))
+            cur.execute(
+                "SELECT * FROM gymnast WHERE gymnast_id = ?", (id,)
+            )
             if not cur.fetchone():
-                error_message = "Error: id does not exist."
-
+                error_message = "Error: ID does not exist."
 
             conn.close()
 
-    return render_template('gymnast.html', regdata=regdata, error_message=error_message, delete_error=delete_error)
+    return render_template(
+        "gymnast.html", regdata=regdata,
+        error_message=error_message, delete_error=delete_error
+    )
 
 
-#creating a page to add scores
-@app.route('/addscores')
+@app.route("/addscores")
 def scores():
-
-    #viewing gymnasts that are in the database
-    conn = sqlite3.connect('database.db')  
+    conn = sqlite3.connect("database.db")
     cur = conn.cursor()
     cur.execute("SELECT * FROM gymnast")
     gymdata = cur.fetchall()
     conn.close()
 
-    #Adding Scores
-    form1 = None
-    form2 = None
-    form3 = None
-    form4 = None
-    if len(request.args) > 0:
-        #getting the data needed from the users input
-        form1 = request.args.get('addgymnastid')
-        form2 = request.args.get('app')
-        form3 = request.args.get('add-escore')
-        form4 = request.args.get('add-dscore')
-        if form1 is not None and form2 is not None and form3 is not None and form4 is not None:
-            conn = sqlite3.connect('database.db')
+    form1 = form2 = form3 = form4 = None
+    if request.args:
+        form1 = request.args.get("addgymnastid")
+        form2 = request.args.get("app")
+        form3 = request.args.get("add-escore")
+        form4 = request.args.get("add-dscore")
+        if form1 and form2 and form3 and form4:
+            conn = sqlite3.connect("database.db")
             cur = conn.cursor()
-            sql = ("INSERT INTO score (gymnast_id, apparatus_id, escore, dscore) VALUES (?,?,?,?)")
-            cur.execute(sql, (form1, form2, form3, form4))
+            cur.execute(
+                "INSERT INTO score (gymnast_id, apparatus_id, escore, dscore) "
+                "VALUES (?, ?, ?, ?)",
+                (form1, form2, form3, form4),
+            )
             conn.commit()
             conn.close()
 
-    #Viewing the registered scores
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect("database.db")
     cur = conn.cursor()
-    cur.execute("SELECT score_id, score.gymnast_id, gymnast.gymnast_name, apparatus.apparatus_name, escore, dscore FROM score INNER JOIN gymnast ON score.gymnast_id=gymnast.gymnast_id INNER JOIN apparatus ON score.apparatus_id=apparatus.apparatus_id ")
+    cur.execute(
+        "SELECT score_id, score.gymnast_id, gymnast.gymnast_name, "
+        "apparatus.apparatus_name, escore, dscore FROM score "
+        "INNER JOIN gymnast ON score.gymnast_id=gymnast.gymnast_id "
+        "INNER JOIN apparatus ON score.apparatus_id=apparatus.apparatus_id"
+    )
     scoredata = cur.fetchall()
     conn.close()
 
-    #delete registered scores
-    delete = None
-    delete_error = None
-    if len(request.args) > 0:
-        delete = request.args.get('deleteid')
-        if delete is not None:
-            conn = sqlite3.connect('database.db')
+    delete = delete_error = None
+    if request.args:
+        delete = request.args.get("deleteid")
+        if delete:
+            conn = sqlite3.connect("database.db")
             cur = conn.cursor()
-            # Check if ID exists
             cur.execute("SELECT * FROM score WHERE score_id = ?", (delete,))
             if not cur.fetchone():
-                delete_error = "Error: id does not exist."
-            
-            time.sleep(1)
-            sql = ("DELETE FROM score WHERE score_id = ?")
-            cur.execute(sql, (delete,))
-            conn.commit()
+                delete_error = "Error: ID does not exist."
+            else:
+                time.sleep(1)
+                cur.execute("DELETE FROM score WHERE score_id = ?", (delete,))
+                conn.commit()
             conn.close()
 
-    #Editing the registered scores
-    newform1 = None
-    newform2 = None
-    newform3 = None
-    newform4 = None
-    msg = None
-    if len(request.args) > 0:
-        #Getting the data needed from the users input
-        newform1 = request.args.get('scoreid')
-        newform2 = request.args.get('appid')
-        newform3 = request.args.get('newescore')
-        newform4 = request.args.get('newdscore')
-        if newform1 is not None and newform2 is not None and newform3 is not None and newform4 is not None:
-            conn = sqlite3.connect('database.db')
+    newform1 = newform2 = newform3 = newform4 = msg = None
+    if request.args:
+        newform1 = request.args.get("scoreid")
+        newform2 = request.args.get("appid")
+        newform3 = request.args.get("newescore")
+        newform4 = request.args.get("newdscore")
+        if newform1 and newform2 and newform3 and newform4:
+            conn = sqlite3.connect("database.db")
             cur = conn.cursor()
-            sql = ("UPDATE score SET apparatus_id = ?, escore = ?, dscore = ? WHERE score_id = ?")
-            cur.execute(sql, (newform2, newform3, newform4, newform1))
+            cur.execute(
+                "UPDATE score SET apparatus_id = ?, escore = ?, dscore = ? "
+                "WHERE score_id = ?",
+                (newform2, newform3, newform4, newform1),
+            )
             conn.commit()
-            
-            # Check if ID exists
             cur.execute("SELECT * FROM score WHERE score_id = ?", (newform1,))
             if not cur.fetchone():
-                msg = "Error: Score id does not exist."
+                msg = "Error: Score ID does not exist."
             conn.close()
 
-    return render_template("score.html", gymdata=gymdata, scoredata=scoredata, msg=msg, delete_error=delete_error)
+    return render_template(
+        "score.html",
+        gymdata=gymdata,
+        scoredata=scoredata,
+        msg=msg,
+        delete_error=delete_error
+    )
 
-   
 
-    
-
-#get and order the gymnasts levels in descending order
-@app.route('/scoredata')
+@app.route("/scoredata")
 def scoredata():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect("database.db")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM ( SELECT *, ROW_NUMBER() OVER(PARTITION BY level ORDER BY gymnast_id) AS num FROM gymnast ) AS ranked_gymnasts WHERE num = 1 ORDER BY level;")
+    cur.execute(
+        "SELECT * FROM ( "
+        "SELECT *, ROW_NUMBER() OVER(PARTITION BY level ORDER BY gymnast_id) "
+        "AS num "
+        "FROM gymnast "
+        ") AS ranked_gymnasts "
+        "WHERE num = 1 "
+        "ORDER BY level;"
+    )
+
     levels = cur.fetchall()
-    conn.close() 
-    
+    conn.close()
     return render_template("scoredata.html", levels=levels)
 
-#get the levels in decending order
-@app.route('/scorelead/<level>')
+
+@app.route("/scorelead/<int:level>")
 def level_leaderboard(level):
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect("database.db")
     cur = conn.cursor()
-    cur.execute("SELECT score.* FROM score JOIN gymnast ON score.gymnast_id = gymnast.gymnast_id WHERE gymnast.level = ?", (level,))
-    levels = cur.fetchall()  
 
-     # viewing overall leaderboard
-    conn = sqlite3.connect('database.db')
-    cur = conn.cursor()
-    cur.execute("SELECT score.gymnast_id, gymnast.gymnast_name, SUM(score.dscore + score.escore) AS total FROM gymnast JOIN score ON score.gymnast_id = gymnast.gymnast_id JOIN apparatus ON score.apparatus_id = apparatus.apparatus_id WHERE gymnast.level = ? GROUP BY score.gymnast_id, gymnast.gymnast_name ORDER BY total DESC  ", (level,))
+    cur.execute(
+        "SELECT DISTINCT apparatus.apparatus_id, apparatus.apparatus_name "
+        "FROM apparatus "
+        "JOIN score ON apparatus.apparatus_id = score.apparatus_id "
+        "JOIN gymnast ON score.gymnast_id = gymnast.gymnast_id "
+        "WHERE gymnast.level = ? "
+        "ORDER BY apparatus.apparatus_id",
+        (level,)
+    )
+    apps = cur.fetchall()
+
+    cur.execute(
+        "SELECT score.gymnast_id, gymnast.gymnast_name, "
+        "SUM(score.dscore + score.escore) AS total "
+        "FROM gymnast "
+        "JOIN score ON score.gymnast_id = gymnast.gymnast_id "
+        "WHERE gymnast.level = ? "
+        "GROUP BY score.gymnast_id, gymnast.gymnast_name "
+        "ORDER BY total DESC",
+        (level,)
+    )
     overalldata = cur.fetchall()
-    conn.close()
-    
-    #viewing floor leaderboard
-    conn = sqlite3.connect('database.db')
-    cur = conn.cursor()
-    cur.execute("SELECT score.gymnast_id, gymnast.gymnast_name, score.dscore,score.escore, (score.dscore + score.escore) AS total FROM gymnast JOIN score ON score.gymnast_id = gymnast.gymnast_id JOIN apparatus ON score.apparatus_id = apparatus.apparatus_id WHERE apparatus.apparatus_id = 6 and gymnast.level = ? ORDER BY total DESC ", (level,))
-    floordata = cur.fetchall()
-    conn.close()
 
-    #viewing pommel leaderboard
-    conn = sqlite3.connect('database.db')
-    cur = conn.cursor()
-    cur.execute("SELECT score.gymnast_id, gymnast.gymnast_name, score.dscore,score.escore, (score.dscore + score.escore) AS total FROM gymnast JOIN score ON score.gymnast_id = gymnast.gymnast_id JOIN apparatus ON score.apparatus_id = apparatus.apparatus_id WHERE apparatus.apparatus_id = 5 and gymnast.level = ? ORDER BY total DESC ", (level,))
-    pommeldata = cur.fetchall()
     conn.close()
+    return render_template("scorelead.html",
+                           level=level,
+                           apps=apps,
+                           overalldata=overalldata)
 
-    #viewing rings leaderboard
-    conn = sqlite3.connect('database.db')
+
+@app.route("/apparatuslead/<int:level>/<int:apparatus_id>")
+def apparatus_leaderboard(level, apparatus_id):
+    conn = sqlite3.connect("database.db")
     cur = conn.cursor()
-    cur.execute("SELECT score.gymnast_id, gymnast.gymnast_name, score.dscore,score.escore, (score.dscore + score.escore) AS total FROM gymnast JOIN score ON score.gymnast_id = gymnast.gymnast_id JOIN apparatus ON score.apparatus_id = apparatus.apparatus_id WHERE apparatus.apparatus_id = 4 and gymnast.level = ? ORDER BY total DESC ", (level,))
-    ringsdata = cur.fetchall()
-    conn.close()
 
-    #viewing vault leaderboard
-    conn = sqlite3.connect('database.db')
-    cur = conn.cursor()
-    cur.execute("SELECT score.gymnast_id, gymnast.gymnast_name, score.dscore, score.escore, (score.dscore + score.escore) AS total FROM gymnast JOIN score ON score.gymnast_id = gymnast.gymnast_id JOIN apparatus ON score.apparatus_id = apparatus.apparatus_id WHERE apparatus.apparatus_id = 3 and gymnast.level = ? ORDER BY total DESC ", (level,))
-    vaultdata = cur.fetchall()
-    conn.close()
+    cur.execute(
+        "SELECT score.gymnast_id, gymnast.gymnast_name, "
+        "score.dscore, score.escore, "
+        "(score.dscore + score.escore) AS total "
+        "FROM gymnast "
+        "JOIN score ON score.gymnast_id = gymnast.gymnast_id "
+        "WHERE score.apparatus_id = ? AND gymnast.level = ? "
+        "ORDER BY total DESC",
+        (apparatus_id, level)
+    )
+    data = cur.fetchall()
 
-    #viewing parellel bar leaderboard
-    conn = sqlite3.connect('database.db')
-    cur = conn.cursor()
-    cur.execute("SELECT score.gymnast_id, gymnast.gymnast_name, score.dscore,score.escore, (score.dscore + score.escore) AS total FROM gymnast JOIN score ON score.gymnast_id = gymnast.gymnast_id JOIN apparatus ON score.apparatus_id = apparatus.apparatus_id WHERE apparatus.apparatus_id = 2 and gymnast.level = ? ORDER BY total DESC ", (level,))
-    pbardata = cur.fetchall()
-    conn.close()
+    cur.execute(
+        "SELECT apparatus_name FROM apparatus WHERE apparatus_id = ?",
+        (apparatus_id,)
+    )
+    apparatus_name = cur.fetchone()[0]
 
-    #viewing high bars leaderboard
-    conn = sqlite3.connect('database.db')
-    cur = conn.cursor()
-    cur.execute("SELECT score.gymnast_id, gymnast.gymnast_name, score.dscore, score.escore, (score.dscore + score.escore) AS total FROM gymnast JOIN score ON score.gymnast_id = gymnast.gymnast_id JOIN apparatus ON score.apparatus_id = apparatus.apparatus_id WHERE apparatus.apparatus_id = 1 and gymnast.level = ? ORDER BY total DESC ", (level,))
-    highbardata = cur.fetchall()
-    conn.close()
+    cur.execute(
+        "SELECT DISTINCT apparatus.apparatus_id, apparatus.apparatus_name "
+        "FROM apparatus "
+        "JOIN score ON apparatus.apparatus_id = score.apparatus_id "
+        "JOIN gymnast ON score.gymnast_id = gymnast.gymnast_id "
+        "WHERE gymnast.level = ? "
+        "ORDER BY apparatus.apparatus_id",
+        (level,)
+    )
+    apps = cur.fetchall()
 
-    return render_template("scorelead.html", levels=levels, overalldata=overalldata, floordata=floordata, pommeldata=pommeldata, ringsdata=ringsdata, vaultdata=vaultdata, pbardata=pbardata, highbardata=highbardata )
+    conn.close()
+    return render_template("apparatuslead.html",
+                           level=level,
+                           apparatus_id=apparatus_id,
+                           data=data,
+                           apps=apps,
+                           apparatus_name=apparatus_name)
 
 
 if __name__ == "__main__":
